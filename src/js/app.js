0c2e3f822df5863767e79ca73701c31f7c79a496
ilyawebdevelop <ilia966@tut.by>
2025-06-24 11:19:05 +0300

    slider nachal

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,24 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar } from 'swiper';
 Swiper.use([Navigation, Pagination, Autoplay, Mousewheel, EffectFade, Thumbs, Scrollbar]);
 
-
+// Инициализация слайдера stepSlider
+document.querySelectorAll('.stepSlider').forEach(n => {
+  const mySwiperSteps = new Swiper(n, {
+    slidesPerView: 'auto',
+    spaceBetween: 24,
+    speed: 600,
+    loop: true,
+    centeredSlides: true,
+    navigation: {
+      nextEl: n?.closest('.stepSliderW').querySelector('.navArrowNext'),
+      prevEl: n?.closest('.stepSliderW').querySelector('.navArrowPrev'),
+    },
+    pagination: {
+      el: n?.closest('.stepSliderW').querySelector('.swiper-pagination'),
+      clickable: true,
+      type: 'bullets',
+    },
+  });
+});
 
 
